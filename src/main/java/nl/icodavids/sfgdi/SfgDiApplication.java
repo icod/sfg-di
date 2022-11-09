690edd683dac07d3624119f62bbba51634b90dd1
Ico <ico@icodavids.nl>
2022-11-09 13:31:42 +0100

    Profiles

--- src/main/java/nl/icodavids/sfgdi/SfgDiApplication.java
+++ src/main/java/nl/icodavids/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package nl.icodavids.sfgdi;
 
-import nl.icodavids.sfgdi.controllers.ConstructorBasedController;
-import nl.icodavids.sfgdi.controllers.MyController;
-import nl.icodavids.sfgdi.controllers.PropertyInjectedController;
-import nl.icodavids.sfgdi.controllers.SetterInjectedController;
+import nl.icodavids.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) context.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) context.getBean("myController");
 
